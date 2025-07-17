c89f1e084a882b1c848429647294e9a3ff9614ea
Santiago Sica <santisica29@gmail.com>
2025-07-17 00:15:14 -0300

    create enum class

---
+++ Flashcards/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class Enum
+    {
+        public enum MenuOptions
+        {
+            ViewStacks,
+            ViewFlashcards,
+            Study,
+            ViewStudyData
+        }
+    }
+}
